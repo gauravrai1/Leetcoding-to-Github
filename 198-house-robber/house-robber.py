610e47ee140372e3cf05b4fc2c3e2998821e5837
Gaurav Rai <gauravrai568@gmail.com>
2024-04-25 11:56:10 -0400

    Time: 31 ms (85.06%) | Memory: 16.5 MB (89.72%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        cache = {}
+        maxval = nums[-1]
+
+        for i in range(len(nums)-3, -1, -1):
+            nums[i] = maxval + nums[i]
+            maxval = max(maxval, nums[i+1])
+        
+        return max(nums[0], nums[1])
