b2f3db0037c124476d58c10fbdfc8dc1a8bc2178
Gaurav Rai <gauravrai568@gmail.com>
2024-04-24 21:25:51 -0400

    Time: 766 ms (44.32%) | Memory: 27.6 MB (11.22%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,25 +3,9 @@ class Solution:
 
         maxprofit = 0
         minval = 100001
-        maxval = -1
-        leftarr = [-1] * len(prices)
-        rightarr = [-1] * len(prices)
 
-        for i in range(1, len(prices)):
-            minval = min(minval, prices[i-1])
-            leftarr[i] = minval
+        for p in prices:
+            minval = min(minval, p)
+            maxprofit = max(maxprofit, p-minval)
         
-        for i in range(len(prices)-1, -1, -1):
-            maxval = max(maxval, prices[i])
-            rightarr[i] = maxval
-
-        # print(leftarr)
-        # print(rightarr)
-
-        for i, p in enumerate(prices):
-            if leftarr[i] == -1 or rightarr[i] == -1:
-                continue
-            else:
-                maxprofit = max(maxprofit, rightarr[i]-leftarr[i])
-
         return maxprofit
